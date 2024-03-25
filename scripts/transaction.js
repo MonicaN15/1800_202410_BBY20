27b2272199a0fffcf9c3cacefeb358c489eb3abf
JaskunwarH <jaskunwar.ca@gmail.com>
2024-03-25 16:24:15 -0700

    Update made to the transaction html and js page

--- scripts/transaction.js
+++ scripts/transaction.js
@@ -1,6 +1,44 @@
-function saveTransactionAndRedirect(){
-    let params = new URL(window.location.href) //get the url from the search bar
-    let ID = params.searchParams.get("docID");
-    localStorage.setItem('transactionID', ID);
-    window.location.href = 'transaction.html';
+/**
+ * Function to add a transaction to the database.
+ * Retrieves transaction details from the input fields,
+ * saves them to the database, and redirects to the transaction page.
+ */
+function addTransaction() {
+    console.log("saving transactions and redirecting");
+
+    // Retrieve transaction details from input fields
+    let transactionDate = document.getElementById("transdate").value;
+    let transactionDescription = document.getElementById("transdescription").value;
+    let transactionAmount = parseFloat(document.getElementById("transamount").value);
+    let transactionCategory = document.getElementById("transcategory").value;
+
+    // Log transaction details for debugging
+    console.log(transactionDate, transactionDescription, transactionAmount, transactionCategory);
+
+    // Check if a user is signed in
+    var user = firebase.auth().currentUser;
+    if (user) {
+
+        // Add transaction to the 'transactions' collection in Firestore
+        db.collection("transactions").add({
+            amount: transactionAmount,
+            category: transactionCategory,
+            date: transactionDate,
+            description: transactionDescription,
+        })
+        .then(() => {
+            // On successful addition, log success message and redirect to transaction page
+            console.log("Transaction successfully added");
+            window.location.href = "main.html";
+        })
+        .catch((error) => {
+            // If there's an error adding the transaction, log error message
+            console.error("Error adding transaction: ", error);
+            // Handle error gracefully, e.g., display an error message to the user
+        });
+    } else {
+        // If no user is signed in, redirect to the login page
+        console.log("No user is signed in");
+        window.location.href = 'index.html';
+    }
 }
